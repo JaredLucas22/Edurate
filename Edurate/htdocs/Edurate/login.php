f44529740fc1adcfa16f94ec07053e97c473df88
JaredLucas22 <102346087+JaredLucas22@users.noreply.github.com>
2023-11-24 19:05:20 +0800

    v1

--- Edurate/htdocs/Edurate/login.php
+++ Edurate/htdocs/Edurate/login.php
@@ -61,18 +61,29 @@ $password = "";
 $database = "edurate";
 $port = "3307";
 
-// Create connection
 $connection = mysqli_connect($servername, $username, $password, $database, $port);
 
-// Check connection
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// If connection is successful, echo "Connected successfully"
-if ($connection) {
-    echo "Connected successfully";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $enteredUsername = $_POST["username"];
+    $enteredPassword = $_POST["password"];
+
+    // Fetch user data from the database
+    $query = "SELECT * FROM user WHERE User_Name = '$enteredUsername' AND User_Password = '$enteredPassword'";
+    $result = mysqli_query($connection, $query);
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        $user = mysqli_fetch_assoc($result);
+        echo "Login successful! Welcome, " . $user["First_Name"] . " " . $user["Last_Name"] . ".";
+    } else {
+        echo "Invalid username or password.";
+    }
 }
+
+mysqli_close($connection);
 ?>
 
 
