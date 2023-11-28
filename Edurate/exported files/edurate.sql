8a94a5d383fcf20f7be514fcd2288aa35d425fc6
JaredLucas22 <102346087+JaredLucas22@users.noreply.github.com>
2023-11-28 09:20:05 +0800

    database update

--- Edurate/exported files/edurate.sql
+++ Edurate/exported files/edurate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Generation Time: Nov 27, 2023 at 11:23 PM
+-- Generation Time: Nov 28, 2023 at 02:18 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
