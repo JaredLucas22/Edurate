3338a63f450c4642add8cae2f42e6ef3df30132e
JaredLucas22 <102346087+JaredLucas22@users.noreply.github.com>
2023-11-28 08:15:02 +0800

    v1

--- Edurate/htdocs/Edurate/review.php
+++ Edurate/htdocs/Edurate/review.php
@@ -1,28 +1,38 @@
 <?php
-
-include("sqllogin.php"); // Adjust this to your actual database connection details
-
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+include("sqllogin.php");
 $connection = mysqli_connect($servername, $username, $password, $database, $port);
 
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Assuming you store the user ID in the session during login
-    $reviewerID = $_SESSION["user_id"];
-    $rating = $_POST["rating"];
-    $comment = $_POST["comment"];
-    $subjectID = $_POST["subject"];
+// Assuming you store the user ID in the session during login
+$reviewerID = $_SESSION["user_id"];
 
-    // Insert the review into the database
-    $query = "INSERT INTO ratings (ReviewerID, SubjectID, Rating, Comment) VALUES ('$reviewerID', '$subjectID', '$rating', '$comment')";
+// Fetch reviews for the logged-in user with the User_Name as Subject
+$query = "SELECT r.*, u.User_Name 
+          FROM ratings r
+          INNER JOIN user u ON r.SubjectID = u.User_ID
+          WHERE r.ReviewerID = '$reviewerID'";
+$result = mysqli_query($connection, $query);
 
-    if (mysqli_query($connection, $query)) {
-        echo "Review submitted successfully!";
-    } else {
-        echo "Error: " . $query . "<br>" . mysqli_error($connection);
+if ($result && mysqli_num_rows($result) > 0) {
+    // Display the user's reviews
+    echo "<div class='reviews-container'>";
+    echo "<h2>Your Reviews</h2>";
+    while ($review = mysqli_fetch_assoc($result)) {
+        echo "<div class='review'>";
+        echo "<p><strong>Subject:</strong> " . $review["User_Name"] . "</p>";
+        echo "<p><strong>Rating:</strong> " . $review["Rating"] . "</p>";
+        echo "<p><strong>Comment:</strong> " . $review["Comment"] . "</p>";
+        echo "</div>";
     }
+    echo "</div>";
+} else {
+    echo "You have no reviews yet.";
 }
 
 mysqli_close($connection);
