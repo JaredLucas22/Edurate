2a8f9087bac2a03b6da872ab0723373391e32691
JaredLucas22 <102346087+JaredLucas22@users.noreply.github.com>
2023-12-05 15:01:39 +0800

    for live

--- sqllogin.php
+++ sqllogin.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$database = "edurate";
+$username = "u226250292_6u2XP";
+$password = "Ii8W5RP5/";
+$database = "u226250292_r1pA8";
 $port = "3307";
 ?>
